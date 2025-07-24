2350c2c3aa70d547c85d7e072f279b49f3c4402b
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-24 15:08:23 +0200

    BaseConfiguration idiot proof system

--- src/main/java/com/ignitedev/treasureHunt/config/BaseConfiguration.java
+++ src/main/java/com/ignitedev/treasureHunt/config/BaseConfiguration.java
@@ -1,20 +1,63 @@
 package com.ignitedev.treasureHunt.config;
 
+import com.ignitedev.treasureHunt.util.BaseUtility;
 import lombok.Getter;
+import net.kyori.adventure.text.Component;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.Nullable;
 
 @Getter
 public class BaseConfiguration {
 
-  private final String prefix;
-  private final String foundMessage;
-  private final String noPermissionMessage;
+  private final FileConfiguration fileConfiguration;
+
+  // messages
+
+  private Component prefix;
+  private Component foundMessage;
+  private Component noPermissionMessage;
+
+  // sql
 
   public BaseConfiguration(FileConfiguration configuration) {
-    this.prefix = configuration.getString("messages.prefix", "&7[&6TreasureHunt&7] &f");
-    this.foundMessage = configuration.getString("messages.found", "&aYou found a treasure!");
+    this.fileConfiguration = configuration;
+
+    loadMessages();
+  }
+
+  private void loadMessages() {
+    ConfigurationSection messages = this.fileConfiguration.getConfigurationSection("messages");
+
+    if (messages == null) {
+      messages = this.fileConfiguration.createSection("messages");
+    }
+    this.prefix = getMessage(messages, "prefix", "&7[&6TreasureHunt&7] &f");
+    this.foundMessage = getMessage(messages, "found", "&aYou found a treasure!");
     this.noPermissionMessage =
-        configuration.getString(
-            "messages.noPermission", "&cYou do not have permission to use this command");
+        getMessage(messages, "noPermission", "&cYou do not have permission to use this command");
+  }
+
+  private Component getMessage(
+      ConfigurationSection configurationSection, String path, @Nullable String def) {
+    String string = configurationSection.getString(path);
+
+    if (string == null) {
+      string = def;
+    }
+    return BaseUtility.colorize(string);
+  }
+
+  private Sound getSound(
+      ConfigurationSection configurationSection, String path, @Nullable Sound def) {
+    Sound sound = Registry.SOUNDS.get(NamespacedKey.minecraft(path));
+
+    if (sound == null) {
+      sound = def;
+    }
+    return sound;
   }
 }
