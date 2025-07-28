2117287f1ce1dd606dcaf8023a3fb9bfbce2e903
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-28 18:17:41 +0200

    Listener for admin treasure creation

---
+++ src/main/java/com/ignitedev/treasureHunt/listener/TreasureSelectionListener.java
@@ -0,0 +1,35 @@
+package com.ignitedev.treasureHunt.listener;
+
+import com.ignitedev.treasureHunt.TreasureHunt;
+import com.ignitedev.treasureHunt.util.PlayerSelection;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class TreasureSelectionListener implements Listener {
+
+  private final TreasureHunt plugin;
+
+  public TreasureSelectionListener(TreasureHunt plugin) {
+    this.plugin = plugin;
+  }
+
+  @EventHandler
+  public void onPlayerInteract(PlayerInteractEvent event) {
+    if (event.getAction() != Action.LEFT_CLICK_BLOCK && event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+      return;
+    }
+
+    Player player = event.getPlayer();
+    if (PlayerSelection.hasPendingSelection(player)) {
+      event.setCancelled(true);
+      Block clickedBlock = event.getClickedBlock();
+      if (clickedBlock != null) {
+        PlayerSelection.handleSelection(player, clickedBlock.getLocation());
+      }
+    }
+  }
+}
