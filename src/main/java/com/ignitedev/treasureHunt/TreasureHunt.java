9cc52d16ec47f7faf6175043fc949c6ac6a74fe3
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-28 18:18:18 +0200

    Init main class

--- src/main/java/com/ignitedev/treasureHunt/TreasureHunt.java
+++ src/main/java/com/ignitedev/treasureHunt/TreasureHunt.java
@@ -1,20 +1,45 @@
 package com.ignitedev.treasureHunt;
 
+import com.ignitedev.treasureHunt.command.TreasureCommand;
 import com.ignitedev.treasureHunt.config.BaseConfiguration;
+import com.ignitedev.treasureHunt.database.SQLDatabaseManager;
+import com.ignitedev.treasureHunt.listener.TreasureSelectionListener;
+import com.ignitedev.treasureHunt.repository.TreasureRepository;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TreasureHunt extends JavaPlugin {
+
+  private SQLDatabaseManager sqlDatabaseManager;
 
   @Override
   public void onEnable() {
     saveDefaultConfig();
 
-    BaseConfiguration baseConfiguration = new BaseConfiguration();
+    BaseConfiguration baseConfiguration = new BaseConfiguration(getConfig());
 
+    this.sqlDatabaseManager =
+        new SQLDatabaseManager(
+            baseConfiguration.getHost(),
+            baseConfiguration.getPort(),
+            baseConfiguration.getDatabase(),
+            baseConfiguration.getUsername(),
+            baseConfiguration.getPassword(),
+            this);
+
+    TreasureRepository.initialize(this.sqlDatabaseManager);
+    TreasureRepository.get().loadAllTreasures();
+    
+    // Register listeners
+    getServer().getPluginManager().registerEvents(new TreasureSelectionListener(this), this);
+    
+    // Register commands
+    getCommand("treasure").setExecutor(new TreasureCommand(this, baseConfiguration));
   }
 
   @Override
   public void onDisable() {
-    // Plugin shutdown logic
+    if (this.sqlDatabaseManager != null) {
+      this.sqlDatabaseManager.close();
+    }
   }
 }
