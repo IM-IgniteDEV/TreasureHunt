4cbee8cc059e52783757adf522f217a3b42f5eec
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-29 19:54:18 +0200

    Added location to Treasure

--- src/main/java/com/ignitedev/treasureHunt/base/Treasure.java
+++ src/main/java/com/ignitedev/treasureHunt/base/Treasure.java
@@ -4,12 +4,14 @@ import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 @Getter
 @RequiredArgsConstructor
 public class Treasure {
   private final String id;
+  private final Location location;
   private final List<String> rewardCommands;
 
   public void grantTreasure(Player player) {
